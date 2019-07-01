46e32c0b44fa68af01e489d3b495cdff5c29fc58
Matej Urbas <matej.urbas@gmail.com>
2019-07-01 09:33:10 +0100

    The GET /metrics api now produces actual readings.

--- rpi_dht22_exporter/app.py
+++ rpi_dht22_exporter/app.py
@@ -9,11 +9,7 @@ def create_app(users=None):
 
 
 def main():
-  # import Adafruit_DHT
-  # temperature, humidity = Adafruit_DHT.read_retry(sensor=Adafruit_DHT.DHT22, pin=14) 
-  # print(f"Temperature: {temperature}")
-  # print(f"Humidity: {humidity}")
-  pass
+    pass
 
 
 if __name__ == "__main__":
--- rpi_dht22_exporter/metrics.py
+++ rpi_dht22_exporter/metrics.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+import logging
+from os import environ
+
+from flask import Blueprint, abort
 from flask.json import jsonify
 
 API = Blueprint("metrics", __name__, url_prefix="/metrics")
@@ -6,4 +9,14 @@ API = Blueprint("metrics", __name__, url_prefix="/metrics")
 
 @API.route("")
 def get():
-    return jsonify(temperature=42, humidity=95)
+    try:
+        # pylint: disable=import-error
+        import Adafruit_DHT
+
+        temperature, humidity = Adafruit_DHT.read_retry(
+            sensor=Adafruit_DHT.DHT22, pin=int(environ.get("DHT22_PIN", "14"))
+        )
+        return f"temperature {temperature}\nhumidity {humidity}"
+    except Exception as ex:
+        logging.error("Could not read values from DHT22. Error: %s", ex)
+        return abort(501)
